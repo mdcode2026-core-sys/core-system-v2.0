4bfc3092f606662cb6e6b937bab9194d9d6451ae
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-06-10 15:11:43 +0300

    Create index.ts

---
+++ src/shared/store/index.ts
@@ -0,0 +1,5 @@
+// src/shared/store/index.ts
+export { useAuthStore } from './authStore';
+export { useTenantStore } from './tenantStore';
+export { useQueueStore } from './queueStore';
+export { useUiStore } from './uiStore';
