bf03f648b0d8bbce7ddb84eb0b8444960026c22f
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-06-10 18:14:42 +0300

    Create index.ts

---
+++ src/core/auth/index.ts
@@ -0,0 +1,6 @@
+// src/core/auth/index.ts
+export { AuthProvider, useAuthContext } from './AuthProvider';
+export { useAuth } from './useAuth';
+export { useRole } from './useRole';
+export { RoleGuard } from './RoleGuard';
+export { PinAuthProvider, usePinAuth } from './PinAuthProvider';
