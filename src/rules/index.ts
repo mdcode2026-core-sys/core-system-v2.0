1207ab198172750668618563b7b2948cb42d88be
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-06-10 14:08:14 +0300

    Update index.ts

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,1 +1,9 @@
 export {}
+// src/rules/scoring/index.ts
+// Barrel export for scoring module
+
+export { computeCoreScore, computeCoreScoreBatch, recomputeWithDelta } from './CoreScoreEngine';
+export { DEFAULT_WEIGHTS, getActiveWeights, validateWeights } from './IndicatorWeights';
+export { calculatePqsPenalty, getPqsPenaltyLabel } from './PqsPenaltyCalculator';
+export { computeWeightedScore, isHistoricalStale } from './LtvWeightRule';
+export { classifyPatient, getClassLabel, getClassColor } from './PatientClassifier';
