45109197af5641001e34431e7cba60c2a9feaac1
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-06-10 15:20:12 +0300

    Update index.ts

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,1 +1,12 @@
 export {}
+// src/domain/index.ts
+// Barrel export for domain layer
+
+export * from './tenants/tenants.queries';
+export * from './tenants/tenants.mutations';
+export * from './patients/patients.queries';
+export * from './patients/patients.mutations';
+export * from './sessions/sessions.queries';
+export * from './sessions/sessions.mutations';
+export * from './invoicing/invoicing.queries';
+export * from './invoicing/invoicing.mutations';
