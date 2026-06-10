b7d408378544bf5341df13974e4b03c3302222f9
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-06-10 18:14:12 +0300

    Create index.ts

---
+++ src/core/offline/index.ts
@@ -0,0 +1,7 @@
+// src/core/offline/index.ts
+export { coreDrive } from './CORE_SYSTEM_DRIVE';
+export { mutationQueue } from './MutationQueue';
+export { syncEngine } from './SyncEngine';
+export { conflictResolver } from './ConflictResolver';
+export { useNetworkMonitor } from './NetworkMonitor';
+export type { PendingMutation } from './MutationQueue';
