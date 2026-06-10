8dc68de417d4501a4a54911ba2ec3ab8d822848e
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-06-10 18:07:59 +0300

    Create index.ts

---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,6 @@
+// src/shared/hooks/index.ts
+export { useTenant } from './useTenant';
+export { useQueue } from './useQueue';
+export { useSlaTimer } from './useSlaTimer';
+export { useNetworkStatus } from './useNetworkStatus';
+export { useAuditLog } from './useAuditLog';
