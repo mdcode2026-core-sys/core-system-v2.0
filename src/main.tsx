ecc017a8625f53cc55052ecf8d4dbf7dd58c9a51
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-06-11 13:15:26 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
-import { router } from './router'
-import { AuthProvider } from './core/auth/AuthProvider'
-
-const queryClient = new QueryClient()
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 )
