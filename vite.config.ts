98030f2ebeb6b92481046dd2a2bc068e8c8e20a5
mdcode2026-core-sys <mdcode2026@gmail.com>
2026-06-16 11:46:18 +0000

    Disable PWA to fix API caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: false,
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      }
-    })
+    // PWA disabled to fix API caching:
+    // // VitePWA({
+    // registerType: 'autoUpdate',
+    // manifest: false,
+    // workbox: {
+    // globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+    // }
+    // })
   ],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
